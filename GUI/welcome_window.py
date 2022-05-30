097e089d465430d7c32f708200ed6fddc763b6b5
Caleb Nelson <dizzerin@gmail.com>
2022-05-30 00:32:14 -0700

    Added basic settings window scaffolding

--- GUI/welcome_window.py
+++ GUI/welcome_window.py
@@ -10,6 +10,7 @@ class WelcomeWindow(IWindow):
 
         # Local vars
         self.is_created = False
+        self.settings_window_tag = "settings_window"
 
         # TODO create rounded transparent button style and apply to buttons on this screen
 
@@ -49,9 +50,61 @@ class WelcomeWindow(IWindow):
             dpg.add_static_texture(width=width1, height=height1, default_value=data1, tag="title_image")
             dpg.add_static_texture(width=width2, height=height2, default_value=data2, tag="background_image")
 
-        # Build the window
-        with dpg.window(tag=self.tag(), show=True):
+        # Build settings popup/modal window
+        settings_window_width = 500
+        settings_window_height = 400
+        settings_combo_box_width = 80
+        settings_text_width = 200
+        # Todo (move this somewhere else?)
+        with dpg.window(label="Settings", tag=self.settings_window_tag, show=False, modal=True,
+                        width=settings_window_width, height=settings_window_height,
+                        pos=[int(viewport_width / 2 - settings_window_width / 2),
+                             int(viewport_height / 2 - settings_window_height / 2)]):
+            # Settings options
+            with dpg.table(header_row=False, resizable=False, borders_innerH=False, borders_outerH=False,
+                           borders_innerV=False, borders_outerV=False):
+                dpg.add_table_column(init_width_or_weight=settings_text_width, width_fixed=True)
+                dpg.add_table_column()
 
+                with dpg.table_row():
+                    dpg.add_text("Default Camera Index:")
+                    dpg.add_combo(["Camera " + str(n) for n in range(0, 11)], width=settings_combo_box_width)
+
+            dpg.add_spacer(height=20)
+            dpg.add_text("ADC analog input mapping:")
+            with dpg.table(header_row=False, resizable=False, borders_innerH=False, borders_outerH=False,
+                           borders_innerV=False, borders_outerV=False):
+                dpg.add_table_column(init_width_or_weight=settings_text_width, width_fixed=True)
+                dpg.add_table_column()
+                # Todo make sure the same ADC input isn't selected for multiple things (handle in callback? or on "OK")
+                #   Load defaults from config file
+                #   Save results to config file
+                with dpg.table_row():
+                    dpg.add_text("Temperature 0:")
+                    dpg.add_combo(["ADC" + str(n) for n in range(0, 11)], label="Input", width=settings_combo_box_width)
+                for i in range(0, 5):
+                    with dpg.table_row():
+                        dpg.add_text("Pressure {}:".format(i))
+                        dpg.add_combo(["ADC" + str(n) for n in range(0, 11)], label="Input",
+                                      width=settings_combo_box_width)
+
+            dpg.add_spacer(height=20)
+            dpg.add_text("Default Background")
+            # Todo actually implement this, maybe show available images and make them clickable etc.
+            dpg.add_combo(["Background " + str(n) for n in range(0, 4)], width=120)
+
+            # OK (and Cancel?) buttons
+            # Todo actually implement OK and cancel buttons, cancel doesn't save changes, OK does (also do error checking etc.)
+            dpg.add_spacer(height=20)
+            dpg.add_separator()
+            with dpg.group(horizontal=True):
+                dpg.add_button(label="OK", width=75,
+                               callback=lambda: dpg.configure_item(self.settings_window_tag, show=False))
+                # dpg.add_button(label="Cancel", width=75,
+                #                callback=lambda: dpg.configure_item(self.settings_window_tag, show=False))
+
+        # Build main welcome window
+        with dpg.window(tag=self.tag(), show=True):
             # Add background image
             dpg.add_image("background_image", pos=[0, 0])
 
@@ -68,12 +121,18 @@ class WelcomeWindow(IWindow):
             dpg.add_button(label="Live Session", width=button_width, height=button_height,
                            pos=[int(viewport_width / 2 - button_width / 2), button_y_start + 0 * button_y_spacing],
                            callback=lambda: GUI_manager.change_window(GUI_manager.INITIALIZATION_WINDOW))
+
             dpg.add_button(label="Settings", width=button_width, height=button_height,
                            pos=[int(viewport_width / 2 - button_width / 2), button_y_start + 1 * button_y_spacing],
-                           callback=lambda: GUI_manager.change_window(GUI_manager.LIVE_WINDOW))
-            dpg.add_button(label="About", width=button_width, height=button_height,
+                           callback=lambda: dpg.configure_item(self.settings_window_tag, show=True))
+
+            dpg.add_button(label="About", tag="About", width=button_width, height=button_height,
                            pos=[int(viewport_width / 2 - button_width / 2), button_y_start + 2 * button_y_spacing],
                            callback=lambda: GUI_manager.change_window(GUI_manager.LIVE_WINDOW))
+            with dpg.tooltip(parent="About"):
+                dpg.add_text("Made By: Caleb Nelson")
+                dpg.add_text("Full about page coming later")
+
             dpg.add_button(label="Exit", width=button_width, height=button_height,
                            pos=[int(viewport_width / 2 - button_width / 2), button_y_start + 3 * button_y_spacing],
                            callback=dpg.stop_dearpygui)
