766299b0c2464a57a49e1a1d360531579458b83d
Caleb Nelson <dizzerin@gmail.com>
2022-05-29 22:54:43 -0700

    New background images added

--- GUI/welcome_window.py
+++ GUI/welcome_window.py
@@ -29,7 +29,7 @@ class WelcomeWindow(IWindow):
         # Local vars
         # Note: These are only used for the button on this screen
         # Todo maybe implement some of this as a style and make the text larger and apply the style to these buttons instead
-        button_y_start = viewport_height / 2 + 80
+        button_y_start = viewport_height / 2 + 60
         button_width = 150
         button_height = 35
         button_y_spacing = 60  # Number of vertical pixels between top of one button to top of next only for Welcome Window buttons
@@ -39,7 +39,12 @@ class WelcomeWindow(IWindow):
 
         # Create textures/images (which will later be added to the window)
         width1, height1, channels1, data1 = dpg.load_image(r"Image_Resources/Logo2.png")
-        width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/WelcomeScreenBackground.png")
+        # width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/Red_Room_Red_Text.png")
+        # width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/Red_Room_Blue_Text.png")
+        # width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/Blue_Room_Blue_Text.png")
+        # width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/Blue_Room_Blue_Text_Lots_Of_Roof.png")
+        # width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/Red_Room_Blue_Text_Lots_Of_Roof.png")
+        width2, height2, channels2, data2 = dpg.load_image(r"Image_Resources/Red_Room_Red_Text_Lots_Of_Roof.png")
         with dpg.texture_registry():
             dpg.add_static_texture(width=width1, height=height1, default_value=data1, tag="title_image")
             dpg.add_static_texture(width=width2, height=height2, default_value=data2, tag="background_image")
